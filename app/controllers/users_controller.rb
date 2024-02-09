486c34debb5515c475d15475bffcbb07a5d8c402
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-09 22:22:56 +0000

    working on user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def index
+    matching_users=User.all
+    @list_of_users= matching_users.order({:username => :asc})
     render({ :template => "user_templates/index"})
+  end
+
+  def show
+    url_username=params.fetch("path_username")
+    matching_usernames=User.where({:username => url_username})
+    @the_user=matching_usernames.at(0)
+
+    render({ :template => "user_templates/details"})
   end
 end
